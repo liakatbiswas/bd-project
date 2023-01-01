e52f0d9d407788be11f83144507f5f4e0848af09
Liakat <liakatbiswas@gmail.com>
2023-01-02 00:04:09 +0600

    config done

---
+++ components/Button.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="bg-blue-500">Test</button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<!-- <template>
+  <div>
+    <h1>Index</h1>
+  </div>
+</template> -->
+
+<template>
+  <header>
+    <Button />
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/courses">courses</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
